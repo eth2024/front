c2b8d1eaacb392f0c77cd78a8a51a17f9cda2799
emune216 <than4502@gmail.com>
2024-03-31 01:45:22 +0900

    feat: Add not-found, error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center">
+      <p className="text-60 font-bold">ERROR</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center">
+      <p className="text-60 font-bold">404</p>
+    </div>
+  );
+};
+
+export default NotFound;
